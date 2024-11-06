2358b2fa4cb943b2511310d082596f4eb7247705
ShaiYosef <oliveryosef61@gmail.com>
2024-11-06 12:34:48 -0700

    Animation
    Idle animation and running animation have been added to the player

--- Assets/Scripts/PlayerMovementBehavior.cs
+++ Assets/Scripts/PlayerMovementBehavior.cs
@@ -12,7 +12,25 @@ public class PlayerMovementBehavior : MovementBehavior
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) GetComponent<Animator>().SetBool("Running", true);
+        else GetComponent<Animator>().SetBool("Running", false);
+
+        if (Input.GetKey(KeyCode.Space)) GetComponent<Animator>().SetBool("Jumping", true);
+        else GetComponent<Animator>().SetBool("Jumping", false);
+
     }
 
     private void FixedUpdate()
